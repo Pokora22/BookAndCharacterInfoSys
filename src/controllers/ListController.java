706862bdb232e02ca2dac659b6e20ac5b4433472
Kigaru <jakubster9@gmail.com>
2019-01-02 19:08:20 +0000

    Added barebones FXML for the application

---
+++ src/controllers/ListController.java
@@ -0,0 +1,32 @@
+package controllers;
+
+import javafx.fxml.FXML;
+
+public class ListController {
+
+    @FXML
+    private void add() {    //creates an empty object and
+                            //prompts user to a new window to add parameters
+
+    }
+
+    @FXML
+    private void delete() { //if item selected, delete it
+
+    }
+
+    @FXML
+    private void view() {   //if item selected, view it
+                            //(that window will allow editing)
+
+    }
+
+    @FXML
+    private void goBack() { //go back to the previous window
+
+    }
+    @FXML
+    private void search() { //search for the item from the list
+
+    }
+}
